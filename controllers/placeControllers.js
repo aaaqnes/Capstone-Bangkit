20e25aaabdc72bd65bc2aaa3c54cff519c860f45
aziz2308 <umar.santo1@gmail.com>
2023-06-11 21:40:01 +0700

    Add GetPlaceByLoc

--- controllers/placeControllers.js
+++ controllers/placeControllers.js
@@ -8,7 +8,7 @@ const addPlace = async (req, res) => {
         const places = await firestore.collection('place').doc(place_id);
         const doc = await places.get();
         if(doc.exists) {
-            res.status(400).send('Place Data with the same ID has been saved before!!')
+            res.status(400).send('Place Data with the same ID has been saved before!')
         } else { 
             const place_data = req.body;
             await places.set(place_data);
@@ -58,6 +58,131 @@ const getPlaceByID = async (req, res) => {
     }
 }
 
+const getPlaceByLocJakpus = async (req, res) => {
+    try {
+        const place = await firestore.collection('place').where('place_loc', '>=', 'Jakarta Pusat');
+        const data = await place.get();
+        const placesArray = [];
+        if(data.empty) {
+            console.log(data)
+            res.status(404).send('Place Data with the given location not found!');
+        } else {
+            data.forEach(doc => {
+                const places = new Place(
+                    doc.data().place_id,
+                    doc.data().place_name,
+                    doc.data().place_loc,
+                    doc.data().place_desc
+                );
+                placesArray.push(places);
+            });
+            res.send(placesArray);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const getPlaceByLocJakut = async (req, res) => {
+    try {
+        const place = await firestore.collection('place').where('place_loc', '>=', 'Jakarta Utara');
+        const data = await place.get();
+        const placesArray = [];
+        if(data.empty) {
+            console.log(data)
+            res.status(404).send('Place Data with the given location not found!');
+        } else {
+            data.forEach(doc => {
+                const places = new Place(
+                    doc.data().place_id,
+                    doc.data().place_name,
+                    doc.data().place_loc,
+                    doc.data().place_desc
+                );
+                placesArray.push(places);
+            });
+            res.send(placesArray);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const getPlaceByLocJakbar = async (req, res) => {
+    try {
+        const place = await firestore.collection('place').where('place_loc', '>=', 'Jakarta Barat');
+        const data = await place.get();
+        const placesArray = [];
+        if(data.empty) {
+            console.log(data)
+            res.status(404).send('Place Data with the given location not found!');
+        } else {
+            data.forEach(doc => {
+                const places = new Place(
+                    doc.data().place_id,
+                    doc.data().place_name,
+                    doc.data().place_loc,
+                    doc.data().place_desc
+                );
+                placesArray.push(places);
+            });
+            res.send(placesArray);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const getPlaceByLocJaktim = async (req, res) => {
+    try {
+        const place = await firestore.collection('place').where('place_loc', '>=', 'Jakarta Timur');
+        const data = await place.get();
+        const placesArray = [];
+        if(data.empty) {
+            console.log(data)
+            res.status(404).send('Place Data with the given location not found!');
+        } else {
+            data.forEach(doc => {
+                const places = new Place(
+                    doc.data().place_id,
+                    doc.data().place_name,
+                    doc.data().place_loc,
+                    doc.data().place_desc
+                );
+                placesArray.push(places);
+            });
+            res.send(placesArray);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const getPlaceByLocJaksel = async (req, res) => {
+    try {
+        const place = await firestore.collection('place').where('place_loc', '>=', 'Jakarta Selatan');
+        const data = await place.get();
+        const placesArray = [];
+        if(data.empty) {
+            console.log(data)
+            res.status(404).send('Place Data with the given location not found!');
+        } else {
+            data.forEach(doc => {
+                const places = new Place(
+                    doc.data().place_id,
+                    doc.data().place_name,
+                    doc.data().place_loc,
+                    doc.data().place_desc
+                );
+                placesArray.push(places);
+            });
+            res.send(placesArray);
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
 const updatePlace = async (req, res,) => {
     try {
         const place_id = req.params.place_id;
@@ -86,6 +211,11 @@ module.exports = {
     addPlace,
     getAllPlace,
     getPlaceByID,
+    getPlaceByLocJakpus,
+    getPlaceByLocJakut,
+    getPlaceByLocJakbar,
+    getPlaceByLocJaktim,
+    getPlaceByLocJaksel,
     updatePlace,
     deletePlace,
 };
